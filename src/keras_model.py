285073bc789b15931c22172f3099faf3fd642840
Timofey-D <Timofey.Dankevich@gmail.com>
2022-04-26 11:39:04 +0000

    The class is done

--- src/keras_model.py
+++ src/keras_model.py
@@ -4,16 +4,44 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras import regularizers
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+import tensorflow
+
 
 class Keras:
 
-    def __init__(self):
+    def __init__(self, config, L1=0.01, L2=0.01, dropout=0.5, verbose=0):
 
         self.NN = Sequential()
 
-        self.L1 = 0.01
-        self.L2 = 0.01
-        self.dropout = 0.5
+        self.L1 = L1
+        self.L2 = L2
+        self.dropout = dropout
+    
+        match config:
+            case 1:       
+                if verbose == 1:
+                    print("Configuration {}: without regularizers".format(config))
+                self.configuration()
+            case 2:
+                if verbose == 1:
+                    print("Configuration {}: with regularizer L1 = {}".format(config, self.L1))
+                self.configuration_L1()
+            case 3:
+                if verbose == 1:
+                    print("Configuration {}: with regularizer L2 = {}".format(config, self.L2))
+                self.configuration_L2()
+            case 4:
+                if verbose == 1:
+                    print("Configuration {}: with regularizer dropout = {}".format(config, self.dropout))
+                self.configuration_dropout()
+            case 5:
+                if verbose == 1:
+                    print("Configuration {}: with regularizer L2 = {} and dropout = {}".format(config, self.L2, self.dropout))
+                self.configuration_L2_dropout()
+
+
+    def model_info(self):
+        print(self.NN.summary())
 
 
     def configuration(self):
@@ -25,7 +53,7 @@ class Keras:
 
 
     def configuration_L1(self):
-
+        
         self.NN.add(Dense(1024, activation='relu', kernel_regularizer=regularizers.L1(self.L1)))
         self.NN.add(Dense(512, kernel_regularizer=regularizers.L1(self.L1)))
         self.NN.add(Dense(512, kernel_regularizer=regularizers.L1(self.L1)))
@@ -62,14 +90,14 @@ class Keras:
         self.NN.add(Dense(27, activation='softmax'))
 
 
-    def train(self, train, TRL, validate, VL, batch=32, iteration=100, verbose_v=1):
+    def train(self, train, cat_train_l, valid, cat_valid_l, batch=32, iteration=100, verb=1):
         self.train = self.NN.fit(
                 train, 
-                TRL, 
+                cat_train_l, 
                 batch_size=batch, 
                 epochs=iteration, 
-                verbose=verbose_v, 
-                validation_data=(validate, VL)
+                verbose=verb, 
+                validation_data=(valid, cat_valid_l)
         )
 
     def set_L1(self, value):
@@ -104,9 +132,6 @@ class Keras:
     def get_evaluate(self):
         evaluate = self.NN.evaluate(data, labels)
         return evalute
-
-    def predict(self, data):
-        self.prediction = self.NN.predict(data)
 
     def get_report(self, labels):
         report = classification_report(labels.argmax(axis=1), self.prediction.argmax(axis=1))
