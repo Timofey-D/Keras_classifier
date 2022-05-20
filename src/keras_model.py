7564e131e000f11db3aea605073ce1047a784756
Timofey-D <Timofey.Dankevich@gmail.com>
2022-05-20 17:43:12 +0000

    Deleted the implementation of the network structure

--- src/keras_model.py
+++ src/keras_model.py
@@ -1,104 +1,97 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import regularizers
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from matplotlib import pyplot as plt
 import tensorflow
 
 
 class Keras:
 
-    def __init__(self, config, L1=0.01, L2=0.01, dropout=0.5, verbose=0):
+    L1 = None
+    L2 = None
+    dropout = None
+
+    def __init__(self, train, valid, train_l, valid_l):
 
         self.NN = Sequential()
 
-        self.L1 = L1
-        self.L2 = L2
-        self.dropout = dropout
-    
-        match config:
-            case 1:       
-                if verbose == 1:
-                    print("Configuration {}: without regularization".format(config))
-                self.configuration()
-            case 2:
-                if verbose == 1:
-                    print("Configuration {}: with regularizer L1 = {}".format(config, self.L1))
-                self.configuration_L1()
-            case 3:
-                if verbose == 1:
-                    print("Configuration {}: with regularizer L2 = {}".format(config, self.L2))
-                self.configuration_L2()
-            case 4:
-                if verbose == 1:
-                    print("Configuration {}: with regularizer dropout = {}".format(config, self.dropout))
-                self.configuration_dropout()
-            case 5:
-                if verbose == 1:
-                    print("Configuration {}: with regularizer L2 = {} and dropout = {}".format(config, self.L2, self.dropout))
-                self.configuration_L2_dropout()
+        # Change each value of the array to float
+        self.train = train.astype('float32')
+        self.valid = valid.astype('float32')
 
-
-    def model_info(self):
-        print(self.NN.summary())
-
-
-    def configuration(self):
-
-        self.NN.add(Dense(1024, activation='relu'))
-        self.NN.add(Dense(512))
-        self.NN.add(Dense(512))
-        self.NN.add(Dense(27, activation='softmax'))
-
-
-    def configuration_L1(self):
+        # Change the labels from integer to categorical data
+        self.train_l = to_categorical(train_l)
+        self.valid_l = to_categorical(valid_l)
         
-        self.NN.add(Dense(1024, activation='relu', kernel_regularizer=regularizers.L1(self.L1)))
-        self.NN.add(Dense(512, kernel_regularizer=regularizers.L1(self.L1)))
-        self.NN.add(Dense(512, kernel_regularizer=regularizers.L1(self.L1)))
+    
+    def add_input_layer(self):
+
+        if self.L1 != None:
+            self.NN.add(Dense(1024, activation='relu', kernel_regularizer=regularizers.L1(self.L1)))
+        elif self.L2 != None:
+            self.NN.add(Dense(1024, activation='relu', kernel_regularizer=regularizers.L2(self.L2)))
+        else:
+            self.NN.add(Dense(1024, activation='relu'))
+
+
+    def add_internal_layer(self):
+
+        if self.L1 != None:
+            self.NN.add(Dense(512, kernel_regularizer=regularizers.L1(self.L1)))
+        elif self.L2 != None:
+            self.NN.add(Dense(512, kernel_regularizer=regularizers.L2(self.L2)))
+        else:
+            self.NN.add(Dense(512))
+
+
+    def add_output_layer(self):
         self.NN.add(Dense(27, activation='softmax'))
 
 
-    def configuration_L2(self):
-
-        self.NN.add(Dense(1024, activation='relu', kernel_regularizer=regularizers.L2(self.L2)))
-        self.NN.add(Dense(512, kernel_regularizer=regularizers.L2(self.L2)))
-        self.NN.add(Dense(512, kernel_regularizer=regularizers.L2(self.L2)))
-        self.NN.add(Dense(27, activation='softmax'))
-
-
-    def configuration_dropout(self):
-
-        self.NN.add(Dense(1024, activation='relu'))
+    def add_dropout_layer(self):
         self.NN.add(Dropout(self.dropout))
-        self.NN.add(Dense(512))
-        self.NN.add(Dropout(self.dropout))
-        self.NN.add(Dense(512))
-        self.NN.add(Dropout(self.dropout))
-        self.NN.add(Dense(27, activation='softmax'))
 
 
-    def configuration_L2_dropout(self):
-
-        self.NN.add(Dense(1024, activation='relu', kernel_regularizer=regularizers.L2(self.L2)))
-        self.NN.add(Dropout(self.dropout))
-        self.NN.add(Dense(512, kernel_regularizer=regularizers.L2(self.L2)))
-        self.NN.add(Dropout(self.dropout))
-        self.NN.add(Dense(512, kernel_regularizer=regularizers.L2(self.L2)))
-        self.NN.add(Dropout(self.dropout))
-        self.NN.add(Dense(27, activation='softmax'))
-
-
-    def train(self, train, cat_train_l, valid, cat_valid_l, batch=32, iteration=100, verb=1):
+    def train_network(self, batch=32, iteration=100, verb=1):
         self.train = self.NN.fit(
-                train, 
-                cat_train_l, 
+                self.train, 
+                self.train_l, 
                 batch_size=batch, 
                 epochs=iteration, 
                 verbose=verb, 
-                validation_data=(valid, cat_valid_l)
+                validation_data=(self.valid, self.valid_l)
         )
+
+    
+    def get_report(self):
+        report = dict()
+
+        prediction = self.NN.predict(self.valid)
+        report.update( {'prediction' : prediction} )
+
+        [loss, accuracy] = self.NN.evaluate(self.valid, self.valid_l)
+        report.update( {'accuracy' : accuracy} )
+        report.update( {'loss' : loss} )
+
+        label_x1 = self.valid_l.argmax(axis=1)
+        classification = classification_report(label_x1, prediction.argmax(axis=1))
+        report.update( {'classification report' : classification} )
+
+        conf_matrix = confusion_matrix(label_x1, prediction.argmax(axis=1))
+        report.update( {'confusion matrix' : conf_matrix} )
+        
+        return report
+
+
+    def info_model(self):
+        self.NN.summary()
+
+    def compilation(self):
+        self.NN.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
     def set_L1(self, value):
         self.L1 = value
@@ -109,17 +102,6 @@ class Keras:
     def set_dropout(self, value):
         self.dropout = value
 
-    def get_evaluate(self, data, labels):
-        [loss, accuracy] = self.NN.evaluate(data, labels)
-        print("Evaluation result on Test Data : Loss = {}, accuracy = {:.2%}".format(loss, accuracy))
-
-    def predict(self, data):
-        self.prediction = self.NN.predict(data)
-
-    def compilation(self):
-        self.NN.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-        #self.NN.compile(loss='categorical_crossentropy', metrics=['accuracy'])
-
     def get_L1(self):
         return self.L1
 
@@ -129,21 +111,18 @@ class Keras:
     def get_dropout(self):
         return self.dropout
 
-    def get_evaluate(self, data, labels):
-        evaluate = self.NN.evaluate(data, labels)
-        return evaluate
-
-    def get_report(self, labels):
-        report = classification_report(labels.argmax(axis=1), self.prediction.argmax(axis=1))
-        return report
-
-    def get_confusion_matrix(self, labels): 
-        conf_matrix = confusion_matrix(labels.argmax(axis=1), self.prediction.argmax(axis=1))
-        return conf_matrix
-
-    def get_neural_network(self):
+    def get_NN(self):
         return self.NN
 
     def get_train(self):
         return self.train
+
+    def print_evaluate(train, value='loss'):
+        plt.plot(train.history['accuracy'])
+        plt.plot(train.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
 
