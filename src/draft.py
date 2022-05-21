40291f77555ab48467f55117a28acb4e686fa01f
Timofey-D <Timofey.Dankevich@gmail.com>
2022-05-21 13:01:31 +0000

    Added the Output directory

--- src/draft.py
+++
@@ -1,8 +0,0 @@
-from tensorflow import keras
-
-train_ds = keras.utils.image_dataset_from_directory(
-    directory='training_data/',
-    labels='inferred',
-    label_mode='categorical',
-    batch_size=32,
-    image_size=(256, 256))
